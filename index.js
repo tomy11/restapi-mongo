f796806262b18288d2c443f279d62891e02f9c20
tomy11 <ohnemotive74@gmail.com>
2020-12-28 00:30:10 +0700

    add cors

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("Connection Successful!");
 });
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
+  return next()
+});
 app.use('/', Routers);
  
 app.listen(port, function () {
