f704528bc7a8dc81a16da016e7edf99bf3cf034e
tomy11 <ohnemotive74@gmail.com>
2020-12-28 00:35:22 +0700

    delete import model

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const Auth = require('../models/auth.model');
 const authController = require('../controller/auth.controller');
 const router = express.Router();
 
